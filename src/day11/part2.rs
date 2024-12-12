801e313cc302f1ebbcab33c0dc7040eb64623875
Robert Safin <robertsafin98@gmail.com>
2024-12-12 12:44:10 +0800

    re

--- src/day11/part2.rs
+++ src/day11/part2.rs
@@ -6,37 +6,35 @@ pub fn solution() -> i64 {
         .trim()
         .to_string();
 
-    let stones: Vec<i64> = input.split(" ").map(|x| x.parse().unwrap()).collect();
+    let stones: Vec<i64> = input.split(" ").map(|n| n.parse().unwrap()).collect();
 
-    let mut cache: HashMap<i64, i64> = stones.into_iter().fold(HashMap::new(), |mut acc, stone| {
-        *acc.entry(stone).or_insert(0) += 1;
-        acc
-    });
+    let mut stone_counts: HashMap<i64, i64> = HashMap::new();
+
+    for stone in stones {
+        *stone_counts.entry(stone).or_insert(0) += 1;
+    }
 
     for _ in 0..75 {
-        cache = cache
-            .into_iter()
-            .fold(HashMap::new(), |mut new_cache, (stone, count)| {
-                match stone {
-                    0 => {
-                        *new_cache.entry(1).or_insert(0) += count;
-                    }
-                    _ if stone.to_string().len() % 2 == 0 => {
-                        let digits = stone.to_string();
-                        let mid = digits.len() / 2;
-                        let left: i64 = digits[..mid].parse().unwrap();
-                        let right: i64 = digits[mid..].parse().unwrap();
-                        *new_cache.entry(left).or_insert(0) += count;
-                        *new_cache.entry(right).or_insert(0) += count;
-                    }
-                    _ => {
-                        *new_cache.entry(stone * 2024).or_insert(0) += count;
-                    }
-                }
-                new_cache
-            });
+        let mut new_stone_counts: HashMap<i64, i64> = HashMap::new();
+
+        for (&stone, &count) in &stone_counts {
+            if stone == 0 {
+                *new_stone_counts.entry(1).or_insert(0) += count;
+            } else if stone.to_string().len() % 2 == 0 {
+                let digits = stone.to_string();
+                let half_len = digits.len() / 2;
+                let left: i64 = digits[..half_len].parse().unwrap();
+                let right: i64 = digits[half_len..].parse().unwrap();
+                *new_stone_counts.entry(left).or_insert(0) += count;
+                *new_stone_counts.entry(right).or_insert(0) += count;
+            } else {
+                *new_stone_counts.entry(stone * 2024).or_insert(0) += count;
+            }
+        }
+
+        stone_counts = new_stone_counts;
     }
-    let out: i64 = cache.values().sum();
-    println!("{}", out);
-    return out;
+
+    println!("Total stones: {}", stone_counts.values().sum::<i64>());
+    stone_counts.values().sum()
 }
